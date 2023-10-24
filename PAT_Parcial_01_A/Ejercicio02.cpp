a5b45b3245ec21549780a83f029a975d258b4d67
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-24 07:51:57 -0600

    Update Ejercicio02.cpp

--- PAT_Parcial_01_A/Ejercicio02.cpp
+++ PAT_Parcial_01_A/Ejercicio02.cpp
@@ -2,5 +2,27 @@
 
 int Ejercicio02::countStudents(vector<char>* students, vector<char>* sandwiches)
 {
-    return 0;
+    int studentIndex = 0; 
+    int sandwichIndex = 0; 
+    int studentsLeft = students->size(); 
+
+    while (sandwichIndex < sandwiches->size() && studentsLeft > 0)
+    {
+        if ((*students)[studentIndex] == (*sandwiches)[sandwichIndex]) 
+        {
+            studentIndex++;
+            sandwichIndex++;
+            studentsLeft--;
+        } else 
+        {
+            studentIndex++;
+            
+            if (studentIndex >= students->size()) 
+            {
+                studentIndex = 0;
+            }
+        }
+    }
+
+    return studentsLeft;
 }
