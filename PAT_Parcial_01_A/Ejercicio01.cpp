c95f6de7f2c1839887d6a8ba490efa043492ca3d
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-10-24 07:34:39 -0600

    Update Ejercicio01.cpp

--- PAT_Parcial_01_A/Ejercicio01.cpp
+++ PAT_Parcial_01_A/Ejercicio01.cpp
@@ -2,5 +2,40 @@
 
 Node<char>* Ejercicio01::remove(Node<char>* head, char value)
 {
-    return nullptr;
+    if (head == nullptr)
+    {
+        return nullptr;
+    }
+
+    while (head != nullptr && head->value == value)
+    {
+        Node<char>* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    if (head == nullptr)
+    {
+        return nullptr;
+    }
+
+    Node<char>* current = head;
+    Node<char>* previous = current;
+
+    while (current != nullptr) 
+    {
+        if (current->value == value) 
+        {
+            Node<char>* temp = current;
+            previous->next = current->next;
+            current = current->next;
+            delete temp;
+        } else
+        {
+            previous = current;
+            current = current->next;
+        }
+    }
+
+    return head;
 }
