48414959ab88e867a56703e6423800d5c1c0bd93
josesinbob <112579166+josesinbob@users.noreply.github.com>
2023-10-24 08:09:43 -0600

    Update Ejercicio03.cpp
    finalizado

--- PAT_Parcial_01_A/Ejercicio03.cpp
+++ PAT_Parcial_01_A/Ejercicio03.cpp
@@ -1,23 +1,41 @@
 #include "Ejercicio03.h"
+#include <algorithm>
 
-MinStack::MinStack()
+MinStack::MinStack(): head(nullptr) 
 {
 }
 
 void MinStack::push(int value)
 {
+    if (head == nullptr) {
+        head = new Node(value, value, nullptr);
+    }
+    else {
+        head = new Node(value, std::min(value, head->min), head);
+    }
 }
 
 void MinStack::pop()
 {
+    if (head != nullptr) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
 }
 
 int MinStack::top()
 {
+    if (head != nullptr) {
+        return head->value;
+    }
 	return 0;
 }
 
 int MinStack::getMin()
 {
+    if (head != nullptr) {
+        return head->min;
+    }
 	return 0;
 }
